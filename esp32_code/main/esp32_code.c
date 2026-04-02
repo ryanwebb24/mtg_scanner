cc6a0561f031827a8dc37de8fecb3c5e5d98840c
ryanwebb24 <ryanwebb2444@gmail.com>
2026-04-01 22:43:17 -0600

    working uart just need to start using rs-485 and then hook up to pi for testing

--- esp32_code/main/esp32_code.c
+++ esp32_code/main/esp32_code.c
@@ -1,13 +1,16 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
 
 #define LED_PIN GPIO_NUM_2
 #define BUTTON_PIN GPIO_NUM_15
 #define TX_PIN GPIO_NUM_16
 #define RX_PIN GPIO_NUM_17
+#define BUFFER_SIZE 1024
+#define UART_PORT UART_NUM_1
 
-void uart_setup() {
+void uart_setup(void) {
   uart_config_t uart_config = {
     .baud_rate = 115200,
     .data_bits = UART_DATA_8_BITS,
@@ -16,9 +19,12 @@ void uart_setup() {
     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
   };
 
-  uart_driver_install();
-  uart_param_config(&uart_config);
-  
+
+  uart_param_config(UART_PORT, &uart_config);
+ 
+  uart_set_pin(UART_PORT, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+  uart_driver_install(UART_PORT, BUFFER_SIZE, BUFFER_SIZE, 0, NULL, 0);
 
 }
 
@@ -31,6 +37,8 @@ void app_main(void) {
       .intr_type = GPIO_INTR_DISABLE,
     };
     gpio_config(&config);
+
+    uart_setup();
 
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
@@ -46,5 +54,13 @@ void app_main(void) {
         vTaskDelay(delay / portTICK_PERIOD_MS);
         gpio_set_level(LED_PIN, 0);
         vTaskDelay(delay / portTICK_PERIOD_MS);
+        uart_write_bytes(UART_PORT, "hello\n", 6);
+        uint8_t data[BUFFER_SIZE];
+        
+        int len = uart_read_bytes(UART_PORT, data, BUFFER_SIZE, 20 / portTICK_PERIOD_MS);
+        if (len > 0) {
+          data[len] = 0; // null terminate so you can print as string
+          printf("received: %s", data);
+        }
     }
 }
